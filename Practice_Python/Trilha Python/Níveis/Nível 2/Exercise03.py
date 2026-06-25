88807b0ede68fb0bd0f7103fbe2d7f84a2e30dc0
Francisco Junior <fgsj.dev@outlook.com>
2026-06-25 18:18:55 -0300

    File: inserindo novo exercício de aprendizagem em Python
    Conversor de temperatura C° -> F°

---
+++ Practice_Python/Trilha Python/Níveis/Nível 2/Exercise03.py
@@ -0,0 +1,10 @@
+"""
+Conversão de temperaturas
+Leia uma temperatura em Celsius e converta para Fahrenheit (F = C * 9/5 + 32).
+"""
+
+# Variables
+temperature_celsius = int(input("Celsius: "))
+
+# Printing
+print(f"The {temperature_celsius} C° -> F° {temperature_celsius * 9/5 + 32}")
