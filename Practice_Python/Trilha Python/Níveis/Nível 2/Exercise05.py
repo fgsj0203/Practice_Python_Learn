3168d5de5bde4cf505d97417c45213cc48c89a46
Francisco Junior <fgsj.dev@outlook.com>
2026-06-25 20:52:11 -0300

    File: calculando perímetro do retângulo

---
+++ Practice_Python/Trilha Python/Níveis/Nível 2/Exercise05.py
@@ -0,0 +1,11 @@
+"""
+Perímetro do retângulo
+Leia a base e a altura de um retângulo. Calcule seu perímetro (2 * (base + altura)).
+"""
+
+# Variables
+base_value = float(input("Base: "))
+height_value = float(input("Height: "))
+
+# Printing value
+print(f"The perimeter of rectangle: {(2 * (base_value + height_value))}")
