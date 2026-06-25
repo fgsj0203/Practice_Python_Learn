cea92b8a44b94b1375fa6565e853736267790579
Francisco Junior <fgsj.dev@outlook.com>
2026-06-25 18:15:01 -0300

    File: inserindo novo exercício de prática em Python

---
+++ Practice_Python/Trilha Python/Níveis/Nível 2/Exercise02.py
@@ -0,0 +1,11 @@
+"""
+Área do círculo
+Leia o raio de um círculo, calcule sua área (π * raio²) e exiba. Use π = 3.14159.
+"""
+
+# Variables
+PI = 3.14159
+raid_circle = float(input("Raid of circle: "))
+
+# Printing
+print(f"The area of circle is {(PI * (raid_circle ** 2))} cm²")
